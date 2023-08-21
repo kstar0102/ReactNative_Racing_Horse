a12ec3d64cd3335bb08238c7c4e4a3cc48c2de25
SuperSmile Dev <konya.mitsuo@gmail.com>
2023-08-21 00:30:58 -0700

    delete commit raceregister backsave action

--- src/store/actions/ReacRegister/RaceRegisterBackSaveAction.js
+++ src/store/actions/ReacRegister/RaceRegisterBackSaveAction.js
@@ -3,39 +3,12 @@ import axios from "axios";
 import { API } from "../../../utils/config";
 import Toast from "react-native-root-toast";
 
-// export const raceData = (data) => {
-//     return {
-//         type: GET_RACE_CORSE_DATA,
-//         payload: data,
-//     }
-// };
-// export const prizeData = (data) => {
-//     return {
-//         type: GET_RACE_PRIZE_DATA,
-//         payload: data,
-//     }
-// };
-
-// export const jockeysData = (data) => {
-//     return {
-//         type: GET_RACE_JOCKEYS_DATA,
-//         payload: data,
-//     }
-// };
-
 export const raceRegisterData = (data) => {
   return {
     type: GET_RACE_REGISTER_DATA,
     payload: data,
   };
 };
-
-// export const userData = (user) => {
-//   return {
-//     type: LOGIN_USER_DATA,
-//     payload: user,
-//   };
-// };
 
 export function RaceRegisterBackSaveAction(backSaveData) {
   return async (dispatch, getState) => {
